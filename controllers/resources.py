be3414b36385ef2311b46d0794fdf1a4ca333be5
jeydi243 <ilungakadiongo@gmail.com>
2022-12-05 16:38:21 +0200

    replace - by =

--- controllers/resources.py
+++ controllers/resources.py
@@ -2,13 +2,15 @@ from fastapi import APIRouter, File, Form, UploadFile
 from fastapi.exceptions import RequestValidationError, ValidationError
 from rich import print
 
+from services.resource_service import ResourceService
+
 
 router = APIRouter(
     prefix="/resources",
     tags=["management"],
     responses={404: {"description": "Not found methods"}},
 )
-resource_service - ResourceService()
+resource_service = ResourceService()
 
 @router.get("")
 def get_employees():
---
+++ services/gridfs.py
@@ -0,0 +1,6 @@
+from pymongo import MongoClient
+import gridfs
+
+
+db = MongoClient().gridfs_example
+fs = gridfs.GridFS(db)
